5f6f0506bf71a3ac6996dc215387cd8bcc4812ad
mesutKoc77 <mkoc1367@gmail.com>
2022-10-12 19:37:55 +0200

    guncel

---
+++ src/main/java/day04_xpath/C04_CssSelector.java
@@ -0,0 +1,29 @@
+package day04_xpath;
+
+public class C04_CssSelector {
+    public static void main(String[] args) {
+        /*
+        CssSelector 8. locater dır ve x path e cok benzer.
+        xpath=//tagName[@attributeName='attributeValue']
+        cssSelcetor=tagName[attributeName='attributeValue']
+
+        xpath daha guclu
+        xpath ile yazılan locater lar html kodldarında aratabiliyourz.
+
+        css selector un avanatajı
+
+        eger cssselector olustururken id kullaniyorsaniz
+
+          xpath : //tagname[@id='attributeValue']
+          cssSelector : "#attributeValue"
+
+
+          eger css selector olusturkrn class attribute'u kullaniyorsaniz
+          xpath : //tagname[@class='attributeValue']
+          cssSelector : ".attributeValue"
+
+
+
+         */
+    }
+}
