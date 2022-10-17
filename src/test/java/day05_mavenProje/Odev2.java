8e86409b482b11be177a3bdd5ba8d4afebdf4d61
mesutKoc77 <mkoc1367@gmail.com>
2022-10-17 20:41:52 +0200

    guncel

--- src/test/java/day05_mavenProje/Odev2.java
+++ src/test/java/day05_mavenProje/Odev2.java
@@ -1,5 +1,14 @@
 package day05_mavenProje;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.hc.core5.util.Asserts;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
 public class Odev2 {
       /*
     // 1. "https://www.saucedemo.com" Adresine gidin
@@ -13,4 +22,31 @@ public class Odev2 {
 // 9. Sayfayi kapatin
 // 10.Collapse
      */
+
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver","src/resources/chromedriver.exe");
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.get("https://www.saucedemo.com");
+        driver.findElement(By.xpath("//input[@placeholder='Username']")).sendKeys("standard_user");
+        driver.findElement(By.xpath("(//input[@class='input_error form_input'])[2]")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+        WebElement theFirstProduct=driver.findElement(By.xpath("(//div[@class='inventory_item_name'])[1]"));
+        String stringTheFirst=theFirstProduct.getText();
+        theFirstProduct.click();
+        driver.findElement(By.id("add-to-cart-sauce-labs-backpack")).click();
+        Thread.sleep(5000);
+        driver.findElement(By.className("shopping_cart_link")).click();
+        if(driver.getPageSource().contains(stringTheFirst)) System.out.println("Sepette TEST PASSED");
+        else System.out.println("sepetteDegil TEST FAILED");
+
+        Thread.sleep(5000);
+
+
+
+        driver.close();
+
+
+    }
 }
